dd97b4199c3054de6e4d5f927fba9abd78b94af5
Practice <mmska928@gmail.com>
2024-03-13 19:55:35 +0300

    migrate

--- main/models.py
+++ main/models.py
@@ -5,6 +5,5 @@ class Product(models.Model):
     title = models.CharField(max_length=255, blank=False)
     description = models.TextField()
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    
+    discount_price = models.DecimalField(max_digits=12, decimal_places=2, null=True)
     count = models.IntegerField(default=0)
-
