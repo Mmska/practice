ef64540eb1b7fe803253b5f9076858628d8d8650
Practice <mmska928@gmail.com>
2024-03-13 21:34:49 +0300

    migrate

--- app/urls.py
+++ app/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from main.urls import urlpatterns as main_urls
+from django.conf.urls.static import static
 
-    
+from app.settings import MEDIA_ROOT, MEDIA_URL
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_urls)),
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from main.models import Product
+
 
 def index_view(request: HttpRequest):
-    return HttpResponse(render(request, 'products.html', {}))
+    products = Product.objects.all()
+
+    return HttpResponse(render(request, 'products.html', {
+        'products': products
+    }))
