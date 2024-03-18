917b7a168b9a77ad5abf8cb0429a72ffe7520260
Practice <mmska928@gmail.com>
2024-03-18 21:35:30 +0300

    Migrate

--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from main.models import Product
+from .models import Product
 
 
 def index_view(request: HttpRequest):
-    products = Product.objects.all()
+    products = Product.objects.filter(is_active=True)
+    products = products.order_by('-count')
 
     return HttpResponse(render(request, 'products.html', {
         'products': products
